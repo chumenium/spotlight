0591fc2cfba71686256227701b0cc340477f1cce
酒井 智史 <236118@kcs-learning.local>
2025-10-23 11:20:59 +0900

    android設定

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,9 +24,5 @@ tasks.register<Delete>("clean") {
 }
 
 plugins {
-  // ...
-
-  // Add the dependency for the Google services Gradle plugin
-  id("com.google.gms.google-services") version "4.4.4" apply false
-
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
