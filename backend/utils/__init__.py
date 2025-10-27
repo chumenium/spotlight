f73a67416e94fcead82032598178d0345036ebd3
ちつゆ <ovs236118@gmail.com>
2025-10-27 11:15:23 +0900

    Delete backend directory

--- backend/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""ユーティリティモジュール"""
-from .auth import jwt_required, generate_jwt_token, decode_jwt_token, verify_google_token
-
-__all__ = ['jwt_required', 'generate_jwt_token', 'decode_jwt_token', 'verify_google_token']
-
