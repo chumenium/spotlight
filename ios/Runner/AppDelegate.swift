8043e86f4ae46d7ad739df1b79aa9fbd6b41f1f9
Sakai Satoshi <ovs236118@gmail.com>
2025-12-03 01:47:39 +0900

    ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,22 @@
-import Flutter
 import UIKit
+import Flutter
+import FirebaseCore
+import FirebaseMessaging
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // Firebase 初期化
+    FirebaseApp.configure()
+
+    // FlutterFire 自動設定
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
